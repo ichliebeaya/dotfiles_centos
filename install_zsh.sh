f4052bc7691e51e2a01afe2dda74fc5fab06991a
root <root@wordpress.localdomain>
2017-05-04 18:20:14 +0900

    add download dein to install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -12,3 +12,6 @@ git clone git://github.com/zsh-users/zaw.git
   
 #copy .zshrc .zshenv
 sh ~/dotfiles_centos/dotfileLinks.sh
+
+#download dein
+git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
