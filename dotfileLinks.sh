4150c6284c18483800e19831db1b8d23bfa9b88e
root <root@ip-172-31-20-64.us-east-2.compute.internal>
2017-06-02 09:31:10 -0400

    change link settings in dotfileLinks.sh

--- dotfileLinks.sh
+++ dotfileLinks.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles_centos/.vimrc ~/.vimrc
 ln -sf ~/dotfiles_centos/.vim ~/.vim
-if [ "$SHELL" = "/bin/bash" ];then
-	ln -sf ~/dotfiles_centos/.bash_profile ~/.bash_profile
-	ln -sf ~/dotfiles_centos/.bashrc ~/.bashrc
-else if [ "$SHELL" = "/bin/zsh" ];then
-	ln -sf ~/dotfiles_centos/.zshrc ~/.zshrc
-	ln -sf ~/dotfiles_centos/.zshenv ~/.zshenv
-     fi
-fi
+ln -sf ~/dotfiles_centos/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles_centos/.bashrc ~/.bashrc
+ln -sf ~/dotfiles_centos/.zshrc ~/.zshrc
+ln -sf ~/dotfiles_centos/.zshenv ~/.zshenv
